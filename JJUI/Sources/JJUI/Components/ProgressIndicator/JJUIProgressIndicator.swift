507b5a429efe24b9865d0ddf1acbbb0f5fa534e3
javff <juan.vasquez@mercadolibre.cl>
2021-12-28 17:07:49 -0300

    add workspace

--- JJUI/Sources/JJUI/Components/ProgressIndicator/JJUIProgressIndicator.swift
+++ JJUI/Sources/JJUI/Components/ProgressIndicator/JJUIProgressIndicator.swift
@@ -21,69 +21,43 @@ public struct JJUIProgressIndicator: View {
 }
 
 struct CircleProgressBar: View {
-    @State private var spinnerStart: CGFloat = 0.0
-    @State private var spinnerEndS1: CGFloat = 0.03
-    @State private var rotationDegreeS1 = initialDegree
+    @State private var animateStrokeStart = false
+    @State private var animateStrokeEnd = true
+    @State private var isRotating = true
 
-    private let rotationTime: Double = 0.75
-    private let animationTime: Double = 1.4
-    private let fullRotation: Angle = .degrees(360)
-    private static let initialDegree: Angle = .degrees(270)
-    
     let configuration: ProgressIndicatorConfiguration
   
     var body: some View {
         ZStack {
             Circle()
-            //TODO: parametrizar estos valores para la version finita de CircleProgressBar //
                 .stroke(lineWidth: configuration.progressLineWidth)
                 .foregroundColor(.clear)
-            
             Circle()
-                .trim(from: spinnerStart, to: spinnerEndS1)
+                .trim(from: animateStrokeStart ? 1/3 : 1/9, to: animateStrokeEnd ? 2/5 : 1)
                 .stroke(style: StrokeStyle(lineWidth: configuration.progressLineWidth, lineCap: .round, lineJoin: .round))
                 .foregroundColor(configuration.ringColor)
-                .rotationEffect(rotationDegreeS1)
-                .onAppear {
+                .rotationEffect(.degrees(isRotating ? 360 : 0))
+                .onAppear() {
                     
-                    animateSpinner(initAnimation: true)
-                    Timer.scheduledTimer(withTimeInterval: animationTime, repeats: true) { (mainTimer) in
-                        animateSpinner()
+                    withAnimation(.linear(duration: 1).repeatForever(autoreverses: false))
+                    {
+                        self.isRotating.toggle()
+                    }
+                    
+                    withAnimation(.linear(duration: 1).delay(0.5).repeatForever(autoreverses: true))
+                    {
+                        self.animateStrokeStart.toggle()
+                    }
+                    
+                    withAnimation(.linear(duration: 1).delay(1).repeatForever(autoreverses: true))
+                    {
+                        self.animateStrokeEnd.toggle()
                     }
                 }
         }
         .frame(width: configuration.size.width, height: configuration.size.height)
         .padding()
     }
-    
-    // MARK: Animation methods
-    func animateSpinner(with duration: Double, completion: @escaping (() -> Void)) {
-        Timer.scheduledTimer(withTimeInterval: duration, repeats: false) { _ in
-            withAnimation(Animation.easeInOut(duration: rotationTime)) {
-                completion()
-            }
-        }
-    }
-    
-    func animateSpinner(initAnimation: Bool = false) {
-        //TODO: Issue en tiempo de la animacion, despues de la primera vuelta espera un ciclo.
-        let firstAnimationTime = initAnimation ? 0 : self.rotationTime
-        let secondAnimationTime = initAnimation ? rotationTime : rotationTime * 2
-        let lastAnimationTime = initAnimation ? rotationTime - 0.025 :  (rotationTime * 2) - 0.025
-        
-        animateSpinner(with: firstAnimationTime) {
-            spinnerEndS1 = 0.97
-        }
-
-        animateSpinner(with: secondAnimationTime) {
-            spinnerEndS1 = 0.03
-        }
-
-        animateSpinner(with: lastAnimationTime) {
-            rotationDegreeS1 += fullRotation
-        }
-    }
-
 }
 
 struct JJUIProgressIndicator_Previews: PreviewProvider {
